0e6253fb54131f9aae89d19b09626603cd5db2df
pablo <remagforce@gmail.com>
2019-05-18 07:47:55 -0600

    ciclo while

---
+++ index.py
@@ -0,0 +1,47 @@
+"""
+a=1
+
+while(a<6):
+    print(a)
+    a=a+1
+print("programa ejecutado correctamente")
+"""
+"""
+edad = int(input("ingrese su edad porfavor :V"))
+
+while edad < 18:
+    print("usted no es apto para poder entrar a esta pagina vuelva por donde vino ")
+    print()
+    edad = int(input("ingrese su edad porfavor :V"))
+print("bienvenido usted puede psar")
+"""
+"""
+print("hola")
+print("")
+ 
+numeros=1
+
+while numeros >=1:
+    print(numeros)
+    numeros=numeros+1
+    if numeros >=100:    
+        break 
+        print("programa terminado")
+print("gracias esperamos vuelva pronto")
+
+print("espere un momento")
+print("XD")
+print("")
+print("porfavor escribe un numero")
+numero=int(input("que ingrese un numero positivo porfis XD"))
+while numero < 0:
+    print("este no es un numero posistivo")
+    numero=int(input("que ingrese un numero positivo porfis XD"))
+print("gracias nene")
+"""
+
+i=0
+
+while i <=10:
+    print("hola we")
+    i=i+1
